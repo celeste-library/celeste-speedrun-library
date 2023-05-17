076c8fd8a337c5306b70611efa46066bd39597a6
cusher <30704914+cusher@users.noreply.github.com>
2023-05-16 21:55:09 -1000

    Add CORS support to server

--- server/app.py
+++ server/app.py
@@ -1,8 +1,10 @@
 import connexion
 from connexion.resolver import RelativeResolver
+from flask_cors import CORS
 
 app = connexion.FlaskApp(__name__, specification_dir='../api/', options={'swagger_ui': False})
 app.add_api('api.yaml', resolver=RelativeResolver('service'))
+CORS(app.app)
 
 if __name__ == '__main__':
     app.run()
