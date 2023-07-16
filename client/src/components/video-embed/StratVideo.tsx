c48853740abde3a617a700f42fb3eed98f75199f
vaz <v@tryptid.com>
2023-07-16 02:52:29 -0700

    The strats have videos! Styling is borderline acceptable maybe

---
+++ client/src/components/video-embed/StratVideo.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {VideoJS} from './VideoJS'
+import './DiscordVideo.css';
+
+interface StratVideoProps {
+  url: string;
+  framerate?: number; // TODO: use it later
+}
+
+export function StratVideo({url, framerate}: StratVideoProps) {
+  const options = {
+    autoplay: false,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    loop: true,
+
+    sources: [{src: url}],
+  };
+
+  return (
+    <div className="strat-video">
+      <VideoJS options={{...options}}/>
+    </div>
+  );
+}
