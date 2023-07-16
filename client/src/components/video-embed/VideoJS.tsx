da41340efa8705fe9b4f64c6f99b35978155770d
vaz <v@tryptid.com>
2023-07-15 18:54:13 -0700

    Add video.js and VideoJS component

---
+++ client/src/components/video-embed/VideoJS.tsx
@@ -0,0 +1,45 @@
+import {useState, useEffect, useRef} from 'react';
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css';
+
+
+// TODO: receive and use framerate
+interface Props {
+  options: videojs.PlayerOptions;
+  onReady?: (player: videojs.Player) => void;
+}
+
+/*
+ * This is a generic component for a video.js player.
+ * Reference: https://videojs.com/guides/react/
+ */
+export function VideoJS({options, onReady}: Props) {
+  const videoRef = useRef<HTMLDivElement>(null);
+  //const [player, setPlayer] = useState<null | videojs.Player>(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      // create the player
+      const videoJSElement = document.createElement('video-js');
+      videoJSElement.classList.add('vjs-big-play-centered');
+      videoRef.current.appendChild(videoJSElement);
+
+      const player = videojs(videoJSElement, options, () => {
+        onReady && onReady(player);
+      });
+
+      return () => {
+        if (player && !player.isDisposed()) {
+          player.dispose();
+        }
+      };
+    }
+  }, [options, onReady, videoRef]);
+
+  // Outer div wrapper makes React Strict Mode happy:
+  return (
+    <div data-vjs-player>
+      <div ref={videoRef}/>
+    </div>
+  );
+}
