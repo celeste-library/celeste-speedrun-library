b1b8f3f9b91459f26ca05e854d6d6b19ade92ab0
cusher <30704914+cusher@users.noreply.github.com>
2023-07-16 20:31:23 -1000

    Embed image tags with hardcoded location

--- client/src/components/strat-detail/StratDetail.tsx
+++ client/src/components/strat-detail/StratDetail.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {ReactElement} from 'react';
 import {Strat} from '../../generated';
 import {Notes} from '../note/Notes';
+import './StratDetail.css';
 
 interface Props {
   strat: Strat;
 }
 
+function stratDescriptionParser(description: string): (string | ReactElement)[] {
+  return description.split(/(\[img].*?\[\/img])/)
+      .map(item => {
+        if (item.startsWith('[img]') && item.endsWith('[/img]')) {
+          const filename = item.replace('[img]', '').replace('[/img]', '');
+          const url = `https://media.celestespeed.run/strats/${filename}`;
+          return <img className='inline-media' src={url} alt={filename}/>;
+        }
+        return <p>{item}</p>;
+      });
+}
+
 export function StratDetail({strat}: Props) {
   return (
-      <div>
-        <p>{strat.description}</p>
+      <div className='description-container'>
+        {stratDescriptionParser(strat.description ?? '')}
         {strat.notes && <Notes notes={strat.notes}></Notes>}
       </div>
   );
